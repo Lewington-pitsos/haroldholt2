655a2218636997eae62d4baff3dc9c096824bb21
Samuel Klett Navarro <samuelklettnavarro@gmail.com>
2021-10-28 20:10:51 +1100

    Add files via upload
    '''keep this file in the same folder than twitter1 oauth and twurl
    change the keys and secrets to your codes
    '''
    also change the import statements to hidden1 !!

---
+++ hidden1.py
@@ -0,0 +1,10 @@
+# hidden.py ------   Keep this file separate
+'''keep this file in the same folder than twitter1 oauth and twurl
+change the keys and secrets to your codes
+'''
+
+def oauth():
+    return {"consumer_key": "MBha6D8OUedUqZwzSuku0wWGT",
+            "consumer_secret": " ",
+            "token_key": "2479049365-6SHkD0W7ZHo1gmAr1KI7drpcAhEREGB8FAcSinz",
+            "token_secret": " "}
